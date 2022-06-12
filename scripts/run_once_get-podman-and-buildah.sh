ca8562c03b21eaab3ba9d76d3b1c88bc793a44f4
double-em <8335996+double-em@users.noreply.github.com>
2022-06-12 11:56:35 +0200

    Add run_once for podman install

---
+++ scripts/run_once_get-podman-and-buildah.sh
@@ -0,0 +1,10 @@
+sudo pacman -S podman podman-docker podman-dnsname buildah fuse-overlayfs;
+
+sysctl kernel.unprivileged_userns_clone
+
+sudo touch /etc/subuid /etc/subgid
+
+USERNAME=$(whoami)
+sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USERNAME
+
+podman system migrate
