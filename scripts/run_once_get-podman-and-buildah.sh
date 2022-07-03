1f4e9aeb5b143370a3427a342fe4193a287dab3e
double-em <8335996+double-em@users.noreply.github.com>
2022-07-03 14:07:55 +0200

    Add arch requirement for podman

--- scripts/run_once_get-podman-and-buildah.sh
+++ scripts/run_once_get-podman-and-buildah.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+{{ if eq .chezmoi.os "arch" -}}
 
 sudo pacman -S podman podman-docker podman-dnsname buildah fuse-overlayfs;
 
@@ -10,3 +11,5 @@ USERNAME=$(whoami)
 sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USERNAME
 
 podman system migrate
+
+{{ end -}}
